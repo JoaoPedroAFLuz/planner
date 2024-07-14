2b9c4295bace024ad5c077798619b999eace990a
João Pedro Fortunato <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-07-14 14:48:59 -0300

    criada rota de atualizacao da viagem

--- src/main/java/br/com/joaopedroafluz/planner/trip/TripController.java
+++ src/main/java/br/com/joaopedroafluz/planner/trip/TripController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @RestController
@@ -16,9 +18,9 @@ public class TripController {
     private final TripRepository tripRepository;
     private final ParticipantService participantService;
 
-    @GetMapping("/{code}")
-    public ResponseEntity<Trip> findTripByCode(@PathVariable("code") UUID code) {
-        var trip = tripRepository.findByCode(code);
+    @GetMapping("/{tripCode}")
+    public ResponseEntity<Trip> findTripByCode(@PathVariable("tripCode") UUID tripCode) {
+        var trip = tripRepository.findByCode(tripCode);
 
         return trip.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
@@ -26,7 +28,7 @@ public class TripController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
-        Trip newTrip = new Trip(payload);
+        var newTrip = new Trip(payload);
 
         tripRepository.save(newTrip);
 
@@ -34,6 +36,26 @@ public class TripController {
         participantService.triggerConfirmationEmailToParticipant(newTrip.getCode());
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getCode()));
+    }
+
+    @PutMapping("/{tripCode}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable("tripCode") UUID tripCode,
+                                           @RequestBody TripRequestPayload payload) {
+        var trip = tripRepository.findByCode(tripCode);
+
+        if (trip.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        trip.get().setOwnerName(payload.ownerName());
+        trip.get().setOwnerEmail(payload.ownerEmail());
+        trip.get().setDestination(payload.destination());
+        trip.get().setStartsAt(LocalDateTime.parse(payload.startsAt(), DateTimeFormatter.ISO_DATE_TIME));
+        trip.get().setEndsAt(LocalDateTime.parse(payload.endsAt(), DateTimeFormatter.ISO_DATE_TIME));
+
+        tripRepository.save(trip.get());
+
+        return ResponseEntity.ok(trip.get());
     }
 
 }
